3cfc158473837c666687bfbd00ea1055cafcdc03
MadMadMattMan <92753107+MadMadMattMan@users.noreply.github.com>
2025-04-23 12:54:49 +1200

    script created

---
+++ Misc/collapsingObject.gd
@@ -0,0 +1,13 @@
+extends Node
+
+var state: bool = true
+
+
+
+func _physics_process(delta: float) -> void:
+	var collision: bool = false
+	print("something")
+	
+	if collision:
+		print("collided")
+	
