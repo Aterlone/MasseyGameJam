3c3231f881aa92ec81a5780e6825f0959b7dbb0e
MadMadMattMan <92753107+MadMadMattMan@users.noreply.github.com>
2025-04-23 13:51:21 +1200

    Update collapsingObject.gd

--- Misc/collapsingObject.gd
+++ Misc/collapsingObject.gd
@@ -2,12 +2,6 @@ extends Node
 
 var state: bool = true
 
-
-
-func _physics_process(delta: float) -> void:
-	var collision: bool = false
-	print("something")
-	
-	if collision:
-		print("collided")
-	
+func _on_body_entered(body):
+	if body.name == "Player":
+		print("Player collided with this object!")	
