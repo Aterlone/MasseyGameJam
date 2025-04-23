033bd911a4af1504db026602f6d4df4303b72670
robot <robot>
2025-04-23 23:17:58 +0000

    added house_1 background

--- objects/waterdrop.gd
+++ objects/waterdrop.gd
@@ -3,5 +3,5 @@ extends Area2D
 
 func _on_area_entered(area: Area2D) -> void:
 	if area.get_parent().name == "Player":
-		area.get_parent().get_node("Sprite2D").heal()
+		area.get_parent().get_node("Sprite2D").heal(5)
 		queue_free()
