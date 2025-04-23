23443793c655d6caa8bfd33a04297da2d1fc5177
MadMadMattMan <92753107+MadMadMattMan@users.noreply.github.com>
2025-04-23 15:02:19 +1200

    The Pain Update

--- player/playerValues.gd
+++ player/playerValues.gd
@@ -1,7 +1,6 @@
 extends Sprite2D
 
 # Player values
-
 ## Water needed for life
 @export var max_health: int = 100
 @export var current_health: int = 100
@@ -12,12 +11,21 @@ extends Sprite2D
 ## Base damage taken by player
 var extra = 0
 
+## Day night hurt cycle
+@export var daylight: float = 1
+const min_max = Vector2(1, 8)
+var hurt_multi: float = 8
 
-## Time Survived
+## Update
 func _process(delta):
+	## Time Survived
 	score += delta
-	extra += delta
-	if extra > 1:
+	
+	## Hurt
+	hurt_multi = daylight * (min_max.y - min_max.x) + min_max.x
+	
+	extra += delta * hurt_multi;
+	while extra > 1:
 		current_health = max(current_health-floor(extra), 0)
 		extra -= floor(extra)
 		print("Took damage! Health:", current_health)
