4de44a2885fff9dcf53855509376f2ff43d8a21a
MadMadMattMan <92753107+MadMadMattMan@users.noreply.github.com>
2025-04-24 11:52:50 +1200

    Water Bottle + fixes Update

--- Misc/waterDrop.gd
+++ Misc/waterDrop.gd
@@ -3,6 +3,6 @@ extends Area2D
 func _on_body_entered(body):
 	if body.name == "Player":
 		print("Player collided with this object!")
-		body.get_node("Sprite2D").heal(100)
+		body.get_node("Sprite2D").heal(5)
 		
 		queue_free()
