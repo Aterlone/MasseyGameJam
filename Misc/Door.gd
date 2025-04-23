d42e25b3a147d03b7755c9242cce72f8aac55acf
Aterlone <taneahaines@gmail.com>
2025-04-23 17:37:48 +1200

    Player go to door position

--- Misc/Door.gd
+++ Misc/Door.gd
@@ -1,9 +1,41 @@
 extends Area2D
 
 @onready var area_container = get_tree().get_root().get_node("Main/AreaContainer")
+@export_enum(
+	"A",
+	"B",
+	"C",
+	"D",
+	"E",
+	"F",
+	"G",
+	"H",
+	"I",
+	"J",
+	"K",
+	"L",
+	"M",
+	"N",
+	"O",
+	"P",
+	"Q",
+	"R",
+	"S",
+	"T",
+	"U",
+	"V",
+	"W",
+	"X",
+	"Y",
+	"Z",
+) var door_char
+
 var in_house: bool = false
 var player_on_door = 0
 var player
+
+func _ready() -> void:
+	name = "Door" + str(door_char)
 
 func _process(delta: float) -> void:
 	if player_on_door:
@@ -31,4 +63,7 @@ func _on_body_exited(body):
 func scene_change(scene: String):
 	var this_scene = load(scene)
 	area_container.add_child(this_scene.instantiate())
+	
+	player.global_position = area_container.get_child(0).get_node("Door"+str(door_char)).global_position
+
 	area_container.get_child(0).queue_free()
