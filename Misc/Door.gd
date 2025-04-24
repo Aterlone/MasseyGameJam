c487434271e185ffd45dff54dec70710bbbe2da5
robot <robot>
2025-04-24 00:41:36 +0000

    fixed doors

--- Misc/Door.gd
+++ Misc/Door.gd
@@ -30,6 +30,8 @@ extends Area2D
 	"Z",
 ) var door_char
 
+@onready var MAIN = get_tree().get_root().get_child(0)
+
 var player_on_door = 0
 var player
 
@@ -38,17 +40,21 @@ var other_door
 func _ready() -> void:
 	self.name = "Door" + str(door_char)
 
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	if player_on_door:
 		if Input.is_action_just_pressed("ui_accept"):
-			print("SCENE CHANGE")
-			if player.in_house:
-				print("OUT HOUSE")
-				scene_change("res://level_1.tscn")
-			else:
-				print("IN HOUSE" + str(player.in_house))
-				scene_change("res://house_1.tscn")
-			player.get_node("Sprite2D").outside = !player.in_house
+			
+			MAIN.switch_to_next_level(door_char)
+			return
+			
+			#print("SCENE CHANGE")
+			#if player.in_house:
+				#print("OUT HOUSE")
+				#scene_change("res://level_1.tscn")
+			#else:
+				#print("IN HOUSE" + str(player.in_house))
+				#scene_change("res://house_1.tscn")
+			#player.get_node("Sprite2D").outside = !player.in_house
 
 func _on_body_entered(body):
 	player = body;
@@ -61,18 +67,26 @@ func _on_body_exited(body):
 		player_on_door = 0
 	
 	
-## Change scene
-func scene_change(scene: String):
-	var this_scene = load(scene)
-	player.in_house = !player.in_house
-	
-	area_container.add_child(this_scene.instantiate())
-	
-	other_door = area_container.get_child(1).get_node("Door"+str(door_char))
-	other_door.get_node("CollisionShape2D").queue_free()
-
-	player.global_position = other_door.global_position
-	player.get_node("Sprite2D").checkpoint()
-	
-	area_container.get_child(0).queue_free()
+### Change scene
+#func scene_change(scene: String):
+	#var this_scene = load(scene)
+	#player.in_house = !player.in_house
+	#
+	#MAIN.switch_to_next_level(door_char)
+	#return
+	#
+	#area_container.add_child(this_scene.instantiate())
+	#
+	#await get_tree().process_frame
+	#
+	#other_door = area_container.get_child(1).get_node("Door"+str(door_char))
+	#other_door.get_node("CollisionShape2D").queue_free()
+	##print(str())
+#
+	#player.global_position = other_door.global_position
+	#player.get_node("Sprite2D").checkpoint()
+	#
+	#area_container.get_child(0).queue_free()
+	#
+	#print(str(area_container.get_children()))
 	
---
+++ main.gd
@@ -0,0 +1,31 @@
+extends Node2D
+
+var to_outside = true
+
+func switch_to_next_level(new_door_char):
+	to_outside = !to_outside
+	
+	## insert new level
+	for child in $AreaContainer.get_children():
+		child.queue_free()
+	
+	await get_tree().process_frame
+	
+	if to_outside:
+		$AreaContainer.add_child(load("res://level_1.tscn").instantiate())
+	else:
+		$AreaContainer.add_child(load("res://house_1.tscn").instantiate())
+	
+	await get_tree().process_frame
+	
+	var spawn_door = $AreaContainer.get_child(0).get_node("Door" + str(new_door_char))
+	
+	$Player.global_position = spawn_door.global_position
+	spawn_door.get_node("CollisionShape2D").disabled = true
+	$MainCamera.global_position = $Player.global_position
+	
+	$Player.get_node("Sprite2D").outside = to_outside
+	
+	
+	print(str(new_door_char))
+	print(to_outside)
