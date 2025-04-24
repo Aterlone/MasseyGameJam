67dc5a577f8684edc68a4c7a77c728ad649ecfc8
aroyasr <arohakiri@gmail.com>
2025-04-24 12:36:52 +1200

    collapsing platforms now respawn

--- Misc/collapsPlatform.gd
+++ Misc/collapsPlatform.gd
@@ -9,11 +9,19 @@ func _process(delta):
 	time+=1
 	$"../StaticBody2D/Sprite2D".position += Vector2(0, sin(time)*2)
 
-func _on_timer_timeout() -> void:
-	get_node("../StaticBody2D/Sprite2D").visible = false
-	get_node("../StaticBody2D/CollisionShape2D").disabled = true
-	
-
 func _on_area_entered(area: Area2D) -> void:
 	set_process(true)
-	$"../Timer".start(0.7)
+	#HOW LONG THE PLATFORM TAKES TO BREAK
+	$"../breakTimer".start(1) 
+
+func _on_break_timer_timeout() -> void:
+	get_node("../StaticBody2D/Sprite2D").visible = false
+	get_node("../StaticBody2D/CollisionShape2D").disabled = true
+	set_process(false)
+	#HOW LONG THE PLATFORM TAKES TO RESPAWN
+	$"../restoreTimer".start(5.0) 
+
+func _on_restore_timer_timeout() -> void:
+	get_node("../StaticBody2D/Sprite2D").visible = true
+	get_node("../StaticBody2D/CollisionShape2D").disabled = false
+	
