cf68fe1c5379bad44465ecc48b866d6c8a1ad271
MadMadMattMan <92753107+MadMadMattMan@users.noreply.github.com>
2025-04-24 15:10:59 +1200

    Pit fixed

--- Misc/pitOfDoom.gd
+++ Misc/pitOfDoom.gd
@@ -4,24 +4,19 @@ var cam: Node
 var lockPos: Vector2
 var locked: bool = false
 
-@onready var MAIN = get_tree().get_root().get_node("Main")
+#@onready var MAIN = get_tree().get_root().get_node("Main")
 
 ##Update method
-func _process(delta: float) -> void:
-	if (cam == null):
-		cam = MAIN.get_node("MainCamera")
-	
-	if (locked):
-		print("Locked")
-	
-	cam.unlocked = !locked
+#func _process(delta: float) -> void:
+	#if (cam == null):
+		#cam = MAIN.get_node("MainCamera")
 
 ##On entering pit
 func _on_area_entered(area: Node2D):
 	print("Entered")
-	locked = true
+	#cam.unlocked = false
 
 ##On exiting pit
 func _on_area_exited(area: Node2D):
-	locked = false
+	#cam.unlocked = true
 	area.get_parent().get_node("Sprite2D").respawn()
